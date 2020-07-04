860207f046733dd1aae1d476c967fe09e72e5882
Alexander H Lee III <ahlee1026@gmail.com>
2020-07-04 03:48:53 -0400

    hover correctly implemetd started laying with the css to make it prettier

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, setState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar.js';
@@ -8,6 +8,14 @@ import useNavi from './hooks/useSticky';
 function App() {  
   // render() {
     const { isFlow } = useNavi();
+    const [opacity, setOpacity] = useState(0);
+
+    // const mouseOn = () => {
+    //   setOpacity(0.5);
+    // };
+    // const mouseOff = () => {
+    //   setOpacity(0);
+    // };
 
     return (
       <div className="App">
@@ -21,9 +29,24 @@ function App() {
           Ever the free thinker, I have always enjoyed finding creative solutions to new problems. My time working with clients and training others in solutions development has taught me a unique set of skills that put me in a position to disseminate information to end users effectively while also being able to lend my talents to development and support staff and coordinate the interactions between the end user and the work being done behind the scenes. Now that I have completed my Fullstack Web Development Coding Certification from GaTech, I aim to bring this skillset to a new arena and further sharpen my front end and back end skills in MYSQL, HTML, CSS, Javascript, React and Angular just to name a few. I am continuing to hone my craft by sharpening my skill with code reviews, problems solving, web design and discovering newer languages and techniques. I hope to bring my skills and more to an organization and prove myself a great asset while building on my current abilities.
         </p>
         <div className="portfolio">
-        <a className="portfolio-box" href="https://lobo89x.github.io/Word-Guess-Game/">
-          <img src={require("../src/assets/imgs/tenor.gif")} alt="wordguess" width='25%' height='100%'/>
-        </a>
+          <div  className="portfolio-box">
+
+          <a href="https://lobo89x.github.io/Word-Guess-Game/">
+            <div className="imgCaption" style={{opacity: opacity}}>
+              <div className="title">
+                WordGuess: Tardis Edition
+              </div>
+              <div className="desc">
+              A small hangman-esque game that illustrates use of JQuery, HTML, and CSS in concert.
+              <small className="text-muted">::git repo::<a href="https://github.com/lobo89x/Word-Guess-Game.git">here</a></small>
+              </div>
+            </div>
+            <img src={require("../src/assets/imgs/tenor.gif")} alt="wordguess" width='auto' height='100%' onMouseOver={() => setOpacity(0.75)} onMouseOut={() => setOpacity(0)}/>
+          </a>
+          </div>
+          <a className="portfolio-box"></a>
+          <a className="portfolio-box"></a>
+          <a className="portfolio-box"></a>
         </div>
         <div className="contactMe">
           <img src="" alt="contactMe" href="" />
