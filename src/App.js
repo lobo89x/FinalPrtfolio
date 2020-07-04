6674f8bad38c2e3d12cee2d3e3ca10e9534bd05d
Alexander H Lee III <ahlee1026@gmail.com>
2020-07-04 00:24:45 -0400

    adding my projects

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ function App() {
           Ever the free thinker, I have always enjoyed finding creative solutions to new problems. My time working with clients and training others in solutions development has taught me a unique set of skills that put me in a position to disseminate information to end users effectively while also being able to lend my talents to development and support staff and coordinate the interactions between the end user and the work being done behind the scenes. Now that I have completed my Fullstack Web Development Coding Certification from GaTech, I aim to bring this skillset to a new arena and further sharpen my front end and back end skills in MYSQL, HTML, CSS, Javascript, React and Angular just to name a few. I am continuing to hone my craft by sharpening my skill with code reviews, problems solving, web design and discovering newer languages and techniques. I hope to bring my skills and more to an organization and prove myself a great asset while building on my current abilities.
         </p>
         <div className="portfolio">
-          <img src="" alt="portfolio" href="" />
+        <a className="portfolio-box" href="https://lobo89x.github.io/Word-Guess-Game/">
+          <img src={require("../src/assets/imgs/tenor.gif")} alt="wordguess" width='25%' height='100%'/>
+        </a>
         </div>
         <div className="contactMe">
           <img src="" alt="contactMe" href="" />
