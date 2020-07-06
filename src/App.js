a91d945295f70c716fe8b63bc70114f88fd98d3c
Alexander H Lee III <ahlee1026@gmail.com>
2020-07-06 08:42:42 -0400

    other portfolio items added begin work on contact info

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ function App() {
   // render() {
     const { isFlow } = useNavi();
     const [opacity, setOpacity] = useState(0);
+    const [opacity2, setOpacity2] = useState(0);
+    const [opacity3, setOpacity3] = useState(0);
+    const [opacity4, setOpacity4] = useState(0);
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+
+    const handleSubmit = () => {
+      
+    }
 
     // const mouseOn = () => {
     //   setOpacity(0.5);
@@ -30,26 +42,79 @@ function App() {
         </p>
         <div className="portfolio">
           <div  className="portfolio-box">
-
-          <a href="https://lobo89x.github.io/Word-Guess-Game/" onMouseOver={() => setOpacity(0.75)} onMouseOut={() => setOpacity(0)}>
-            <div className="imgCaption" style={{opacity: opacity}}>
-              <div className="title">
-                WordGuess: Tardis Edition
+            <a href="https://lobo89x.github.io/Word-Guess-Game/" onMouseOver={() => setOpacity(0.75)} onMouseOut={() => setOpacity(0)}>
+              <div className="imgCaption" style={{opacity: opacity}}>
+                <div className="title">
+                  WordGuess: Tardis Edition
+                </div>
+                <div className="desc">
+                A small hangman-esque game that illustrates use of JQuery, HTML, and CSS in concert.
+                </div>
+                <small className="text-muted">::git repo::<a href="https://github.com/lobo89x/Word-Guess-Game.git">here</a></small>
               </div>
-              <div className="desc">
-              A small hangman-esque game that illustrates use of JQuery, HTML, and CSS in concert.
-              </div>
-              <small className="text-muted">::git repo::<a href="https://github.com/lobo89x/Word-Guess-Game.git">here</a></small>
-            </div>
-            <img src={require("../src/assets/imgs/tenor.gif")} alt="wordguess" width='100%' height='100%'/>
-          </a>
+              <img src={require("../src/assets/imgs/tenor.gif")} alt="wordguess" width='100%' height='100%'/>
+            </a>
           </div>
-          <a className="portfolio-box"></a>
-          <a className="portfolio-box"></a>
-          <a className="portfolio-box"></a>
+          <div  className="portfolio-box">
+            <a href="https://shielded-headland-00192.herokuapp.com/" onMouseOver={() => setOpacity2(0.75)} onMouseOut={() => setOpacity2(0)}>
+              <div className="imgCaption" style={{opacity: opacity2}}>
+                <div className="title">
+                  TYT NewsScrape
+                </div>
+                <div className="desc">
+                A marriage of Cherrio and Mongo DB that brings you trusted news and podcasts.
+                </div>
+                <small className="text-muted">::git repo::<a href="https://github.com/lobo89x/NewsScrape.git">here</a></small>
+              </div>
+              <img src={require("../src/assets/imgs/avatars-000060717147-7egjya-t500x500.jpg")} alt="wordguess" width='100%' height='100%'/>
+            </a>
+          </div>
+          <div  className="portfolio-box">
+            <a href="https://numbernauts.herokuapp.com/" onMouseOver={() => setOpacity3(0.75)} onMouseOut={() => setOpacity3(0)}>
+              <div className="imgCaption" style={{opacity: opacity3}}>
+                <div className="title">
+                Numbernauts
+                </div>
+                <div className="desc">
+                A react game that is intended to help math practice. It uses React, Hooks, Redux, Socket IO, and Mongo DB.
+                </div>
+                <small className="text-muted">::git repo::<a href="https://github.com/lobo89x/numbernumbergame.git">here</a></small>
+              </div>
+              <img src={require("../src/assets/imgs/player_1.png")} alt="wordguess" width='100%' height='100%'/>
+            </a>
+          </div>
+          <div  className="portfolio-box">
+            <a href="https://photosbylee.herokuapp.com/" onMouseOver={() => setOpacity4(0.75)} onMouseOut={() => setOpacity4(0)}>
+              <div className="imgCaption" style={{opacity: opacity4}}>
+                <div className="title">
+                Photos by Lee
+                </div>
+                <div className="desc">
+                A React phot gallery where I am playing with multiple gallery components and several images from my phone.
+                </div>
+                {/* <small className="text-muted">::git repo::<a href="https://github.com/lobo89x/NewsScrape.git">here</a></small> */}
+              </div>
+              <img src={require("../src/assets/imgs/CoverIg.jpg")} alt="wordguess" width='100%' height='100%'/>
+            </a>
+          </div>
         </div>
         <div className="contactMe">
           <img src="" alt="contactMe" href="" />
+          <form id="contact-form" onSubmit="" method="POST">
+              <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input type="text" className="form-control" />
+              </div>
+              <div className="form-group">
+                  <label htmlFor="exampleInputEmail1">Email address</label>
+                  <input type="email" className="form-control" aria-describedby="emailHelp" />
+              </div>
+              <div className="form-group">
+                  <label htmlFor="message">Message</label>
+                  <textarea className="form-control" rows="5"></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
         </div>
       </div>
     );
