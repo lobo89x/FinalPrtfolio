05e9696183b7c083a2c7b0eb27954c25ddb0f49f
Alexander H Lee III <ahlee1026@gmail.com>
2020-07-08 01:14:52 -0400

    playing with the banner and header a bit  before bed

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -6,15 +6,39 @@ function Ticker() {
     //render(){
         return (
         <div>
-            <h1>Alexander Hillman Lee III</h1>
+            {/* <h1>Alexander Hillman Lee III</h1> */}
             <marquee 
             behavior='scroll' 
             direction='left' 
-            scrollamount='40'
+            scrollamount='25'
             style={{
-                fontSize:'calc(48px + 2vmin)'
+                fontSize:'calc(48px + 2vmin)',
+                color: 'white',
+                paddingTop: '2%'
             }}>
-              Full Stack Dev &bull; Services/Solutions Engineer
+              Javascript  &bull;  Angular  &bull;  React  &bull;  HTML  &bull;  Mongo DB  &bull;  SQL  &bull;  C# 
+            </marquee>
+            <marquee 
+            behavior='scroll' 
+            direction='left' 
+            scrollamount='50'
+            style={{
+                fontSize:'calc(18px + 2vmin)',
+                color: 'white',
+                paddingTop: '2%'
+            }}>
+              Full-Stack Web Dev with 7 yrs’ exp in IT &bull; Penchant for learning new technologies and skills &bull; Graduate of Georgia Institute of Technology Full Stack Development &bull; Well rounded &bull; Team player &bull; Positive energy &bull; Creative problem solver 
+            </marquee>
+            <marquee 
+            behavior='scroll' 
+            direction='left' 
+            scrollamount='18'
+            style={{
+                fontSize:'calc(126px + 2vmin)',
+                color: 'white',
+                paddingTop: '2%'
+            }}>
+              Full Stack Dev &bull; Services/Solutions Engineer &bull; Composition Specialist
             </marquee>
         </div>
         )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,11 @@ function Navbar ({ pageNav }) {
     return (
         <nav className={pageNav ? "navbar nav-flow":"navbar nav-top"}>
             <p className="nav-Title">Alexander H. Lee III</p>
-            <a href="https://www.linkedin.com/in/alexander-lee-3abb1546/">LinkedIn</a>
-            <a href='https://github.com/lobo89x'>Git Hub</a>
-            <a href="../assets/ALEX_LEE_III_FS0403.pdf" download>Resume</a>
+                <a href="https://www.linkedin.com/in/alexander-lee-3abb1546/">LinkedIn</a>
+                <a href='https://github.com/lobo89x'>Git Hub</a>
+                <a href="../assets/ALEX_LEE_III_FS0403.pdf" download>Resume</a>
+            <div>
+            </div>
         </nav>
     )
 }
