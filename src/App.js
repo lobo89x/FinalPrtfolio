daaf9524d4b01632c700ff4de8b01183a55bc7a5
Alexander H Lee III <ahlee1026@gmail.com>
2020-09-03 16:00:49 -0400

    committing the last updates

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
       <div className="App">
           <Navbar 
           pageNav={isFlow}/>
-
+        
         <div className="abtMe">
           <div className="abtMeBox">
             <a href='https://www.linkedin.com/in/alexander-lee-3abb1546/'>
@@ -124,24 +124,6 @@ function App() {
             </a>
           </div>
         </div>
-        {/* <div className="contactMe">
-          <img src="" alt="contactMe" href="" />
-          <form id="contact-form" method="POST">
-              <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input name='name' type="text" className="form-control" />
-              </div>
-              <div className="form-group">
-                  <label htmlFor="exampleInputEmail1">Email address</label>
-                  <input name='email' type="email" className="form-control" aria-describedby="emailHelp" />
-              </div>
-              <div className="form-group">
-                  <label htmlFor="message">Message</label>
-                  <textarea name='message ' className="form-control" rows="5" ></textarea>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-        </div> */}
         <ContactMe />
       </div>
     );
--- src/components/abtMeDesc.js
+++ src/components/abtMeDesc.js
@@ -6,7 +6,6 @@ function Abtm() {
         <div className='container'>
             <div className='oval'></div>
             Ever the free thinker, I have always enjoyed finding creative solutions to new problems. My time working with clients and training others in solutions development has taught me a unique set of skills that put me in a position to disseminate information to end users effectively while also being able to lend my talents to development and support staff and coordinate the interactions between the end user and the work being done behind the scenes. Now that I have completed my Fullstack Web Development Coding Certification from GaTech, I aim to bring this skillset to a new arena and further sharpen my front end and back end skills in MYSQL, HTML, CSS, Javascript, React and Angular just to name a few. I am continuing to hone my craft by sharpening my skill with code reviews, problems solving, web design and discovering newer languages and techniques. I hope to bring my skills and more to an organization and prove myself a great asset while building on my current abilities.
-
         </div>
     )
 }
